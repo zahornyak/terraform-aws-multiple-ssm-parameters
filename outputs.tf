e4111d1e2123c58f14597e7735226df701adc781
Andriy Zahornyak <andriyzzahornyak@gmail.com>
2023-05-30 16:47:44 +0300

    add output for name ssm

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,10 @@ output "parameters_arns" {
   description = "parameters arns map"
   value       = { for k, v in aws_ssm_parameter.this : k => v["arn"] }
 }
+
+output "parameters_name_arns" {
+  description = "parameters arns map with names of parameter"
+  value = {
+    for key, param in aws_ssm_parameter.this : param.name => aws_ssm_parameter.this[key].arn
+  }
+}
