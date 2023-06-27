e8748d76d02956d9969fd4fecbe79eb28260dcd4
Andriy Zahornyak <andriyzzahornyak@gmail.com>
2023-06-27 13:51:07 +0300

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "parameters_ids" {
   description = "parameters ids map"
-  value       = { for k, v in merge(aws_ssm_parameter.this, aws_ssm_parameter.parsed) : k => v["id"] }
+  value       = { for k, v in merge(aws_ssm_parameter.this, aws_ssm_parameter.parsed, aws_ssm_parameter.unlocked) : k => v["id"] }
 }
 
 output "parameters_arns" {
   description = "parameters arns map"
-  value       = { for k, v in merge(aws_ssm_parameter.this, aws_ssm_parameter.parsed) : k => v["arn"] }
+  value       = { for k, v in merge(aws_ssm_parameter.this, aws_ssm_parameter.parsed, aws_ssm_parameter.unlocked) : k => v["arn"] }
 }
 
 output "parameters_name_arns" {
   description = "merged parameters arns map with names of parameter"
   value = {
-    for key, param in merge(aws_ssm_parameter.this, aws_ssm_parameter.parsed) : param.name => param.arn
+    for key, param in merge(aws_ssm_parameter.this, aws_ssm_parameter.parsed, aws_ssm_parameter.unlocked) : param.name => param.arn
   }
 }
